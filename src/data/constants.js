b193421fb57c5731c5f279dcd7e1b7d74a28d3b8
mdnayab <mdnayabkarim141@gmail.com>
2024-10-18 02:04:32 +0530

    debugging

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/mdnayab",
   resume:
-    "https://drive.google.com/file/d/1YBpuVnuRsJKchaKOZIWF8wzbUNHLwHsd/view?usp=drive_link",
+    "https://drive.google.com/file/d/1PExSduP3Y7u6IbpFI2Ibth7MsBCEeWeY/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/md-nayab-karim-922391250",
   twitter: "https://x.com/md_nyb",
   insta: "https://www.instagram.com/md_nayaabb/profilecard/?igsh=NTJsYXZvOGszOTA3",
